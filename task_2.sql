ee5f8621a06b0970757e8aaa0962e566955c4d54
Prince Isaac Ofori <pknox03@gmail.com>
2025-07-07 18:19:39 +0000

    Create tables for alx_book_store database

--- task_2.sql
+++ task_2.sql
@@ -1,13 +1,13 @@
 -- Create the Authors table
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(100) NOT NULL
+    author_name VARCHAR(100)
 );
 
 -- Create the Books table
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
+    title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Books (
 -- Create the Customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(215),
+    customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS Customers (
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
@@ -35,8 +35,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity INT,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-
